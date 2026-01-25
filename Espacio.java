9b36f9fd78d2558f7704e1c53052cd6029bb5281
sunev-venus <venus.semino@gmail.com>
2026-01-24 18:42:58 -0600

    ya no quiero

---
+++ Espacio.java
@@ -0,0 +1,31 @@
+class Espacio {
+// Atributos
+
+private int numero;
+private String estado;
+private Vehículo vehiculoActual;
+
+//Constructores
+public Espacio(int numero, String estado, Vehículo vehiculoActual){
+    this.numero = numero;
+    this.estado = estado;
+    this.vehiculoActual = vehiculoActual;
+}
+
+// Métodos
+public void ocupar(Vehículo vehiculo){
+    // no va a funcionar por la clase de vehículo
+    // Ya se ecupó?
+}
+
+public void liberar(){
+    // Está libre?
+
+}
+
+public boolean estaLibre(){
+    return false;
+
+}
+
+}
---
+++ Ticket.java
@@ -0,0 +1,35 @@
+class Ticket {
+    //Atributos
+    private String folio;
+    private String horaEntrada;
+    private String horaSalida;
+
+    // Constructor parametrizaddo
+    public Ticket(String folio, String horaEntrada, String horaSalida){
+        this.folio = folio;
+        this.horaEntrada = horaEntrada;
+        this.horaSalida = horaSalida;
+    }
+
+    // Métodos
+    public void finalizar(String horaSalida){
+        // Ya se está yendo
+
+    }
+
+    public String obrenerDetalle(){
+        return "";
+
+    }
+
+    public boolean entregado(){
+        return false; /// hellllllppp
+        
+
+    }
+
+
+// no funcioaaaaaakmsdk
+
+    
+}
