a0b1a857a6b454a8f2c0db513420b295e3acb5a7
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2026-01-24 22:26:10 -0600

    Create ticketAs.java

---
+++ ticketAs.java
@@ -0,0 +1,38 @@
+public class Ticket {
+    private String folio;
+    private String horaEntrada;
+    private String horaSalida;
+    private String estado;
+    private Vehiculo vehiculo;
+    private Espacio espacio;
+
+    public Ticket (String folio, String horaEntrada, Vehiculo vehiculo, Espacio espacio){
+        this.folio = folio;
+        this.horaEntrada = horaEntrada;
+        this.horaSalida= "N/A";
+        this.estado = "Abierto";
+        this.vehiculo =vehiculo;
+        this.espacio = espacio;
+    }
+    
+    public static void finalizar (String horaSalida, String estado){
+        horaSalida= horaSalida;
+        estado = "Cerrado";
+    }
+
+    public String obtenerDetalle(){ //funcion esa la que es en una sola linea 
+        if (vehiculo!= null && espacio !=null){
+            String placa = vehiculo.getPlacas();
+            int esp= espacio.getNumero();
+            return "Folio: " + folio + "Placa: "+ placa +"Espacio: "+ esp+" Hora de entrada: " + horaEntrada + " Hora de salida: " + horaSalida + " Estado: " + estado;
+        }
+        else {
+            return null;
+        }
+    }
+
+    public boolean entregado(){
+        return "Cerrado".equals(espacio);
+    }
+    
+}
