6d9b5fe8e190c24413f44fedc98281e851f136ce
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2026-01-24 22:00:44 -0600

    Update SistemaEstacionamiento.java

--- SistemaEstacionamiento.java
+++ SistemaEstacionamiento.java
@@ -1,1 +1,41 @@
+public class SistemaEstacionamiento {
+    private String nombre;
+    private static int totalEntradas=0;
+    private static int totalSalidas=0;
+    private static int consecutivoTicket =0; 
+    private Espacio espacio;
+
+
+    public SistemaEstacionamiento(String nombre){
+        this.nombre = nombre;
+    }
+    public Ticket registrarEntrada(Vehiculo vehiculo, Espacio espacio) {
+
+        espacio.ocupar(vehiculo);
+
+        totalEntradas++;
+
+        consecutivoTicket++;
+        
+        String folio = "T-" + consecutivoTicket;
+        Ticket t = new Ticket(folio, horaEntrada, vehiculo, espacio);
+        System.out.println("[" + nombre + "] Entrada registrada");
+        System.out.println(" " + vehiculo.obtenerResumen());
+        System.out.println(" " + t.obtenerDetalle());
+
+return t;
+
+}
+
+    public void registrarSalidas (){
+      espacio.liberar();
+      totalSalidas++;
+    }
+    public int getTotalEntradas(){
+        return  totalEntradas;
+    }
+    public int getTotalSalidas(){
+        return totalSalidas;
+    }
+}
 
