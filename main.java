09cd2987a53eaa1916dc89d9cc9335149e75ef4d
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2026-01-24 23:51:47 -0600

    Update main.java

--- main.java
+++ main.java
@@ -1,21 +1,37 @@
-public class main {
-    public static void main(String[] args) {
+public class Main {
+    public static void main (String [] args){
+        SistemaEstacionamiento smN1 = new SistemaEstacionamiento("Lucas");
+        SistemaEstacionamiento smN2 = new SistemaEstacionamiento("Paola");
+        SistemaEstacionamiento smN3= new SistemaEstacionamiento("Lulu");
+        Vehiculo v1= new Vehiculo("MYT45-45", "Standar","12:12 pm");
+        Vehiculo v2= new Vehiculo("SDT02-78", "Standar","2:00 pm");
+        Vehiculo v3= new Vehiculo("AOI21-54", "Standar","2:00 pm");
+        
+        
+        
+        Espacio es1= new Espacio (1, "Libre", v1);
+        Espacio es2 = new Espacio(12, " ", v2);
+        Espacio es3 = new Espacio(5, "Libre", v1);
 
-        // Crear 3 objetos
-        // obj. de Clase entidad
-        Ticket tic1 = new Ticket("A001", "10:55", "12:30");
-        //obj. de Clase cooridinadora
-        Espacio es1 = new Espacio("12", "ocupado", Vehículo);
-        Espacio es2 = new Espacio("5", "libre", Vehículo);
+        Ticket tic1 = new Ticket("A001", "12:05", v1, es1);
+        Ticket tic2 = new Ticket("A005", "10:59", v2, es2);
+        Ticket tic3 = new Ticket("A008", "9:10", v3, es3);
+       
 
-        // ejecutar los 4 mensajes
+       System.out.println(v1.obtenerResumen());
+       System.out.println(" \n");
 
-        System.out.println( "Espacio: " + es1.Espacio.estaLibre(tic1));
-        System.out.println("Ticket: " + tic1.Ticket.obrenerDetalle(es1));
-        System.out.println("Espacio: " + es2.Espacio.liberar(tic1));
-        System.out.println("Ticket: " + tic1.Ticket.obtenerDetalle());
-        // No va a funcionar debido a que se necesita la clase de Vehículo
-
-    }
+       System.out.println("======REGISTRO DE ENTRADAS======");
+       System.out.println(smN1.registrarEntrada(v3, es3));
+       System.out.println(" \n");
+       System.out.println(smN2.registrarEntrada(v1, es1));
+       System.out.println(" \n");
+       System.out.println(smN3.registrarEntrada(v2, es2));
+       System.out.println(" \n");
+       //System.out.println(tic3.obtenerDetalle());
     
+       
+        
+    }
 }
+
