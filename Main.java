54045195e97e8de292905c58ed977aa3a87a8be5
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2026-01-26 17:00:17 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,32 +1,33 @@
 public class Main {
     public static void main (String [] args){
 
-        //SistemaEstacioinamiento
-        SistemaEstacionamiento smN1 = new SistemaEstacionamiento("Central");
-    
-        //Vehiculo
+        SistemaEstacionamiento smN1= new SistemaEstacionamiento("Sistema");
         Vehiculo v1= new Vehiculo("MYT45-45", "Standar","12:12 pm");
         Vehiculo v2= new Vehiculo("SDT02-78", "Standar","2:00 pm");
         Vehiculo v3= new Vehiculo("MKOI-95", "Standar","8:00 pm");
+        Vehiculo v4= new Vehiculo();
+        
      
         //Espacio
-        Espacio es1= new Espacio (1, "Libre", null);
-        Espacio es2 = new Espacio(2, "Libre", null);
-        Espacio es3 = new Espacio(3, "Libre", null);
+        Espacio es1= new Espacio (1);
+        Espacio es2 = new Espacio(2);
+        Espacio es3 = new Espacio(3);
+       
 
         System.out.println("======REGISTRO DE ENTRADAS======\n");
         
         Ticket tic1 = smN1.registrarEntrada(v1, es1);
+        System.out.println("\n");
         Ticket tic2 = smN1.registrarEntrada(v2, es2);
+        System.out.println("\n");
         Ticket tic3 = smN1.registrarEntrada(v3, es3);
+     
 
         System.out.println("======REGISTRO DE SALIDAS======\n");
         tic1.finalizar("2:00 am", "Libre");
-        es1.liberar();
-        smN1.registrarSalidas();
-        tic3.finalizar("2:00 am", "Libre");
-        es3.liberar();
-        smN1.registrarSalidas();
+        smN1.registrarSalidas(es1);
+        tic3.finalizar("9:00 am", "Libre");
+        smN1.registrarSalidas(es3);
 
         System.out.println("\n ========RESUMEN==========");
         System.out.println("Total entradas: " + SistemaEstacionamiento.getTotalEntradas());
@@ -38,7 +39,10 @@ public class Main {
 
         System.out.println("========DETALLES DEL VEHICULO==========");
         System.out.println(tic1.obtenerDetalle());
+        System.out.println("\n");
         System.out.println(tic2.obtenerDetalle());
+        System.out.println("\n");
         System.out.println(tic3.obtenerDetalle());
-    }
+    
+}
 }
