170680fdd770bfffb67434bac718ee99316a0859
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2026-02-20 19:45:47 -0600

    sistema actualizado

--- SistemaEstacionamiento.java
+++ SistemaEstacionamiento.java
@@ -1,19 +1,27 @@
-public class SistemaEstacionamiento {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SistemaEstacionamiento implements Ticketable{
     private String nombre;
     private static int totalEntradas=0;
     private static int totalSalidas=0;
     private static int consecutivoTicket =0; 
-    private Espacio espacio;
-
+    private List<Espacio>espacios;
+    private List<Ticket>tickets;
+  
 
     public SistemaEstacionamiento(String nombre){
         this.nombre = nombre;
+        this.espacios= new ArrayList<>();
+        this.tickets= new ArrayList<>();
+        
     }
     public Ticket registrarEntrada(Vehiculo vehiculo, Espacio espacio) {
 
+        if (espacio != null){
+        espacios.add(espacio);
+        }
         espacio.ocupar(vehiculo); //1
-
-        totalEntradas++;
 
         consecutivoTicket++;
         String horaEntrada = vehiculo.getHoraEntrada(); //2
@@ -22,16 +30,28 @@ public class SistemaEstacionamiento {
         System.out.println("[" + nombre + "] Entrada registrada");
         System.out.println(" " + vehiculo.obtenerResumen());
         System.out.println(" " + t.obtenerDetalle());
+        System.out.println(t.entregado());
 
 return t;
 
 }
-
-    public void registrarSalidas (){    
+    public void registrarSalidas (Espacio e, Ticket t, int horas){  
+        e.liberar();
         totalSalidas++;
+        System.out.println("Total salidas: " + getTotalSalidas());
         System.out.println("["+nombre+"] El espacio se ha liberado. Salida registrada");
-      
+    
     }
+
+    public void generarTicket(Ticket t){
+        if (t ==null)  System.out.println(" ");
+        else {
+        tickets.add(t);}
+        totalEntradas++;
+       System.out.println("Tickets registrados: " + getTotalEntradas());
+        
+    }
+
     public static int getTotalEntradas(){
         return  totalEntradas;
     }
