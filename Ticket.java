b3ec9ef57ee059883510d62afa85390e9d0b5522
Eruviel117 <euruviel.marquez@tecdesoftware.edu.mx>
2026-02-22 15:20:01 -0600

    Add totalPagar and horasEstacionado to Ticket

--- Ticket.java
+++ Ticket.java
@@ -1,3 +1,5 @@
+
+
 public class Ticket {
     private String folio;
     private String horaEntrada;
@@ -5,6 +7,8 @@ public class Ticket {
     private String estado;
     private Vehiculo vehiculo;
     private Espacio espacio;
+    private double totalPagar;
+    private int horasEstacionado;
 
     public Ticket (String folio, String horaEntrada, Vehiculo vehiculo, Espacio espacio){
         if (vehiculo == null) throw new IllegalArgumentException("Nombre es obligatorio");
@@ -12,16 +16,21 @@ public class Ticket {
         if (horaEntrada == null) throw new IllegalArgumentException("Nombre es obligatorio");   
         this.folio = folio;
         this.horaEntrada = horaEntrada;
-        this.horaSalida= "N/A";
+        this.horaSalida= "N/A"; // = null
         this.estado = "Abierto";
         this.vehiculo =vehiculo;
         this.espacio = espacio;
+        this.totalPagar = 0.0;
+        
     }
     
     //cerrado
-    public void finalizar (String horaSalida){
+    public void finalizar (String horaSalida, int horas){
+        this.horasEstacionado = horas;
         this.estado = "Cerrado";
-        System.out.println("Hora salida: "+horaSalida + " Hora entrada: "+horaEntrada +" - Estado: "+ this.estado);
+        this.horaSalida = horaSalida;
+        this.totalPagar = vehiculo.calcularTarifa(horas);
+        System.out.println("Hora salida: "+ this.horaSalida + " Hora entrada: "+horaEntrada +" - Estado: "+ this.estado + "horas Estacionado: "+ this.horasEstacionado);
     }
 
     public String obtenerDetalle(){
@@ -29,7 +38,7 @@ public class Ticket {
         if (vehiculo !=null){
             String placa = vehiculo.getPlacas(); //4 //se duplicaba por eso lo elimine al igual que la hotraEntrada//5
 
-            return "Folio: " +folio+". \n"+"PLaca: "+ placa+". \n" +". \n"+ "Hora de salida: " + horaSalida+". \n" + "Estado: " + estado+". \n" + "Situación: " + situacion ;
+            return "Folio: " +folio+". \n"+"PLaca: "+ placa+". \n" +". \n"+ "Hora de salida: " + horaSalida+". \n" + "Estado: " + estado+". \n" + "Situación: " + situacion+". \n"  + "total a pagar:"+  totalPagar ;
         }
             else {
             return "No fueron proporcionados los datos completos";}
