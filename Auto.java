35e22b984b6939e3d909b6427328e3b6883677b5
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2026-02-22 19:28:27 -0600

    Update Auto.java

--- Auto.java
+++ Auto.java
@@ -5,20 +5,20 @@ public class Auto extends Vehiculo {
 
     public Auto(String placa, String horaEntrada, String tipo){
         super(placa, horaEntrada);
+        if (tipo == null) throw new IllegalArgumentException("El tipo de vehiculo es obligatorio");
         this.tipo = tipo;
         
     }
 
     @Override
     public double calcularTarifa(int horas) {
-        
+        if (horas == 0) throw new IllegalArgumentException("Ponga un numero o que sea mayor a 0");
         return horas * 25.00;
     }
 
     @Override
     public String obtenerResumen(){
-        
-        return "El vehiculo con placas: " +placa + " Tipo: "+ tipo +  " Hora de entrada: " + horaEntrada;
+        return "El vehiculo con placas: " + placa + "\n" +"Tipo:  "+ tipo + "\n"+ "Hora de entrada: " + horaEntrada;
     }
 
 
