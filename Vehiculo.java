7b9a71a1a21996ddcf2cb5e0348f0bf97cfa1e1f
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2026-02-22 19:31:50 -0600

    Update Vehiculo.java

--- Vehiculo.java
+++ Vehiculo.java
@@ -5,15 +5,17 @@ abstract class Vehiculo {
 
     //validaciones
     public Vehiculo (String placa, String horaEntrada){
+        if (placa == null) throw new IllegalArgumentException("La placa es obligatoria");
+        if (horaEntrada== null) throw new IllegalArgumentException("La hora de entrada es obligatoria");
         this.horaEntrada = horaEntrada;
         this.placa = placa;
 
     }
 
     public abstract double calcularTarifa(int horas);
-
+    
     public String obtenerResumen(){      
-        return "El vehiculo con placas: " +placa +  "Hora de entrada: " + horaEntrada ;
+        return "El vehiculo con placas: " +placa + "\n" + "Hora de entrada: " + horaEntrada ;
     }
 
     public String getPlacas(){
