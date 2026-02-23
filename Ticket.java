dbff011196aacb86356beeeed03a89aebbb79759
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2026-02-22 19:30:49 -0600

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -1,5 +1,3 @@
-
-
 public class Ticket {
     private String folio;
     private String horaEntrada;
@@ -11,12 +9,14 @@ public class Ticket {
     private int horasEstacionado;
 
     public Ticket (String folio, String horaEntrada, Vehiculo vehiculo, Espacio espacio){
-        if (vehiculo == null) throw new IllegalArgumentException("Nombre es obligatorio");
-        if (folio == null) throw new IllegalArgumentException("Nombre es obligatorio");
-        if (horaEntrada == null) throw new IllegalArgumentException("Nombre es obligatorio");   
+        if (folio == null) throw new IllegalArgumentException("El folio es obligatorio");
+        if (horaEntrada== null) throw new IllegalArgumentException("La hora de entrada es obligatorio");
+        if (vehiculo == null) throw new IllegalArgumentException("El vehiulo es obligatorio");
+        if (espacio== null) throw new IllegalArgumentException("El espacio es obligatorio");
+        
         this.folio = folio;
         this.horaEntrada = horaEntrada;
-        this.horaSalida= "N/A"; // = null
+        this.horaSalida= "N/A"; 
         this.estado = "Abierto";
         this.vehiculo =vehiculo;
         this.espacio = espacio;
@@ -29,16 +29,16 @@ public class Ticket {
         this.horasEstacionado = horas;
         this.estado = "Cerrado";
         this.horaSalida = horaSalida;
+        //Polimorfismo
         this.totalPagar = vehiculo.calcularTarifa(horas);
-        System.out.println("Hora salida: "+ this.horaSalida + " Hora entrada: "+horaEntrada +" - Estado: "+ this.estado + "horas Estacionado: "+ this.horasEstacionado);
+        System.out.println("Hora entrada: "+horaEntrada +"\n"+"Hora salida: "+ this.horaSalida +"\n"+  "Estado: "+ estado + "\n"+"horas Estacionado: "+ this.horasEstacionado);
     }
 
     public String obtenerDetalle(){
         String situacion= entregado()? "Vehiculo recogido" : "Vehiculo estacionado";
         if (vehiculo !=null){
-            String placa = vehiculo.getPlacas(); //4 //se duplicaba por eso lo elimine al igual que la hotraEntrada//5
 
-            return "Folio: " +folio+". \n"+"PLaca: "+ placa+". \n" +". \n"+ "Hora de salida: " + horaSalida+". \n" + "Estado: " + estado+". \n" + "Situación: " + situacion+". \n"  + "total a pagar:"+  totalPagar ;
+            return "Folio: " +folio+" \n" + "Hora de salida: " + horaSalida+" \n" + "Estado: " + estado+". \n" + "Situación: " + situacion+". \n"  + "Total a pagar:"+  totalPagar ;
         }
             else {
             return "No fueron proporcionados los datos completos";}
